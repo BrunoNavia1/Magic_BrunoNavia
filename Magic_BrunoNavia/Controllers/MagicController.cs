dbf9adff74097b527ee309553a96146e363eba33
Bruno Navia <a2020118467@estudiantes.upsa.edu.bo>
2021-10-14 01:39:44 -0400

    Agregar archivos de proyecto.

---
+++ Magic_BrunoNavia/Controllers/MagicController.cs
@@ -0,0 +1,36 @@
+﻿using Magic_BrunoNavia.Data;
+using Magic_BrunoNavia.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magic_BrunoNavia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MagicController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public MagicController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<Suerte>> GetSuerte()
+        {
+            var list = await _context.Suerte.ToListAsync();
+            var max = list.Count;
+            int index = new Random().Next(0, max);
+            var suerte = list[index];
+            if (suerte == null)
+            {
+                return NoContent();
+            }
+            return suerte;
+        }
+    }
+}
---
+++ Magic_BrunoNavia/Models/Suerte.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magic_BrunoNavia.Models
+{
+    public class Suerte
+    {
+        [Key]
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "Debe poner un numero ")]
+        public string FuturoId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        public string Vision { get; set; }
+         [Url]
+        [StringLength(1000, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Enlace { get; set; }
+
+    }
+}
